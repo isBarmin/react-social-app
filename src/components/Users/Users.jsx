530bafc69bbb5bdccfbefcc83846788d2a421435
isBarmin <isBarmin@yandex.ru>
2019-10-10 18:03:10 +0500

    Добавил стили для Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import s from './Users.module.css';
 
 const Users = props => {
   const { users, follow, unfollow } = props;
 
   const usersElements = users.map(user => {
     return (
-      <div className="user-card" key={user.id}>
-        <div className="user-card__side">
-          <img className="user-card__avatar" src={user.photoUrl} alt="" />
+      <div className={s.userCard} key={user.id}>
+        <div className={s.userCard__side}>
+          <div className={s.userCard__avatar}>
+            <img src={user.photoUrl} alt="" />
+          </div>
           {user.followed ? (
             <button onClick={() => unfollow(user.id)}>unfollow</button>
           ) : (
@@ -15,12 +18,12 @@ const Users = props => {
           )}
         </div>
 
-        <div className="user-card__body">
-          <p className="user-card__text">{user.fullName}</p>
-          <p className="user-card__text">
+        <div className={s.userCard__body}>
+          <p className={s.userCard__text}>{user.fullName}</p>
+          <p className={s.userCard__text}>
             {user.location.Country} {user.location.city}
           </p>
-          <p className="user-card__text">{user.status}</p>
+          <p className={s.userCard__text}>{user.status}</p>
         </div>
       </div>
     );
