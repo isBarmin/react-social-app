c4756af9609f46a4c58190710894db586adf6882
isBarmin <isBarmin@yandex.ru>
2019-10-11 14:56:50 +0500

    Преобразовал Users в классовую компоненту

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,40 +1,44 @@
 import React from 'react';
 import s from './Users.module.css';
 
-const Users = props => {
-  const { users, follow, unfollow } = props;
+class Users extends React.Component {
+  renderUsers = () => {
+    return this.props.users.map(user => {
+      const { follow, unfollow } = this.props;
 
-  const usersElements = users.map(user => {
-    return (
-      <div className={s.userCard} key={user.id}>
-        <div className={s.userCard__side}>
-          <div className={s.userCard__avatar}>
-            <img src={user.photoUrl} alt="" />
+      return (
+        <div className={s.userCard} key={user.id}>
+          <div className={s.userCard__side}>
+            <div className={s.userCard__avatar}>
+              <img src={user.photoUrl} alt="" />
+            </div>
+            {user.followed ? (
+              <button onClick={() => unfollow(user.id)}>unfollow</button>
+            ) : (
+              <button onClick={() => follow(user.id)}>follow</button>
+            )}
           </div>
-          {user.followed ? (
-            <button onClick={() => unfollow(user.id)}>unfollow</button>
-          ) : (
-            <button onClick={() => follow(user.id)}>follow</button>
-          )}
-        </div>
 
-        <div className={s.userCard__body}>
-          <p className={s.userCard__text}>{user.fullName}</p>
-          <p className={s.userCard__text}>
-            {user.location.country} {user.location.city}
-          </p>
-          <p className={s.userCard__text}>{user.status}</p>
+          <div className={s.userCard__body}>
+            <p className={s.userCard__text}>{user.fullName}</p>
+            <p className={s.userCard__text}>
+              {user.location.country} {user.location.city}
+            </p>
+            <p className={s.userCard__text}>{user.status}</p>
+          </div>
         </div>
+      );
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Users</h1>
+        {this.renderUsers()}
       </div>
     );
-  });
-
-  return (
-    <div>
-      <h1>Users</h1>
-      {usersElements}
-    </div>
-  );
-};
+  }
+}
 
 export default Users;
