7f1608fc464dbf8515bea1e23a9491017f6a38f7
isBarmin <isBarmin@yandex.ru>
2019-10-14 16:52:11 +0500

    Изменил вёрстку и стили у Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,14 @@ import s from "./Header.module.css";
 const Header = props => {
   return (
     <header className={s.header}>
-      <div className={s.header__logo}>
-        <img src="" alt="Logo" />
+      <div className={s.header__part}>
+        <a className={s.logo}>
+          <img src="" alt="Logo" />
+        </a>
+      </div>
+
+      <div className={`${s.header__part}  ${s.header__part_right}`}>
+        user-block
       </div>
     </header>
   );
